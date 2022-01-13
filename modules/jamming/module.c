ab6cf897277768d90539d759d59f375dd006b67d
RCayre <rcayre@laas.fr>
2022-01-13 10:41:07 +0100

    Updating jamming module

--- modules/jamming/module.c
+++ modules/jamming/module.c
@@ -4,7 +4,7 @@
 #include "hashmap.h"
 #include "malloc.h"
 
-#define DETECTION_INTERVAL 3
+#define DETECTION_INTERVAL 5
 uint32_t interval;
 uint32_t number_of_packets[3];
 uint32_t continuous_jamming_detected;
@@ -20,7 +20,7 @@ void TIME_CALLBACK(jamming)(metrics_t * metrics) {
     interval++;
 
     if (interval % DETECTION_INTERVAL == 0) {
-      log((uint8_t*)number_of_packets,4*3);
+      //log((uint8_t*)number_of_packets,4*3);
       if (number_of_packets[0] == 0) { continuous_jamming_detected = 37; log((uint8_t*)&continuous_jamming_detected,4);}
       if (number_of_packets[1] == 0) {continuous_jamming_detected = 38; log((uint8_t*)&continuous_jamming_detected,4);}
       if (number_of_packets[2] == 0) {continuous_jamming_detected = 39; log((uint8_t*)&continuous_jamming_detected,4);}

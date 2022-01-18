c6deba11710e9be52342c1a42b56af63dc5e225f
RCayre <rcayre@laas.fr>
2022-01-18 14:06:25 +0100

    Improving broadcom wrapper generator and raspi3 wrapper

--- targets/raspi3/wrapper.c
+++ targets/raspi3/wrapper.c
@@ -33,7 +33,7 @@ uint8_t * rx_header_register = (uint8_t *) 0x318b98;
 uint8_t * rx_register = (uint8_t *) 0x370a00;
 uint8_t * status_register = (uint8_t *) 0x318bac;
 uint8_t * channel = (uint8_t *) 0x20efaa;
-uint8_t * hci_callbacks_table = (uint8_t *) 0x86fcc;
+uint8_t * hci_callbacks_table = (uint8_t *) 0x86fd0;
 uint8_t * bd_address = (uint8_t *) 0x201c64;
 /* Global variables used internally by the wrapper */
 uint8_t current_gap_role;

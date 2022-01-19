bc172ba748859c0cacc91c420f6c58ae193ae4f0
RCayre <rcayre@laas.fr>
2022-01-19 22:08:06 +0100

    Improving cyw20735 wrapper

--- targets/cyw20735/wrapper.c
+++ targets/cyw20735/wrapper.c
@@ -163,8 +163,13 @@ void run_hci_command(uint16_t opcode, uint8_t * buffer, uint8_t size) {
 }
 /* Actions API */
 void start_scan() {
-    //uint8_t buffer_params[7] = {0x00,0x00,0x20,0x00,0x20,0x00,0x00};
-    //run_hci_command(0xb, buffer_params, 7);
+    uint8_t bufferoff[2];
+    bufferoff[0] = 0;
+    bufferoff[1] = 0;
+    run_hci_command(0xc, bufferoff, 2);
+
+    uint8_t buffer_params[7] = {0x00,0x00,0x20,0x00,0x20,0x00,0x00};
+    run_hci_command(0xb, buffer_params, 7);
 
     uint8_t buffer[2];
     buffer[0] = 1;

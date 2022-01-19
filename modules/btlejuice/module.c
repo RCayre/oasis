3a4a69bc71089969bc63ee551177129020e2eb13
RCayre <rcayre@laas.fr>
2022-01-19 22:34:00 +0100

    Improving btlejuice detection module

--- modules/btlejuice/module.c
+++ modules/btlejuice/module.c
@@ -13,12 +13,8 @@ void CONN_INIT_CALLBACK(btlejuice)(metrics_t * metrics) {
   }
 }
 
-void CONN_RX_CALLBACK(btlejuice)(metrics_t * metrics) {
-  /*if(!is_scanning && metrics->current_connection->rx_counter < 75) {
-    start_scan();
-    is_scanning = 1;
-  }*/
-  if(is_scanning && metrics->current_connection->rx_counter > 75) {
+void CONN_DELETE_CALLBACK(btlejuice)(metrics_t * metrics) {
+  if(is_scanning) {
     stop_scan();
     is_scanning = 0;
   }
@@ -26,7 +22,6 @@ void CONN_RX_CALLBACK(btlejuice)(metrics_t * metrics) {
 
 void SCAN_CALLBACK(btlejuice)(metrics_t * metrics) {
   if(is_scanning && metrics->current_packet->valid && get_adv_packet_type() == ADV_IND) {
-    //log(metrics->remote_device->address,6);
     bool same = 1;
     uint8_t i = 0;
 

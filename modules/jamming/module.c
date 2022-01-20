fec67b90c7d3c41a454755a41d040c7bd1798043
RCayre <rcayre@laas.fr>
2022-01-21 00:17:45 +0100

    Improving jamming detection module

--- modules/jamming/module.c
+++ modules/jamming/module.c
@@ -21,7 +21,7 @@ void TIME_CALLBACK(jamming)(metrics_t * metrics) {
   if (metrics->local_device->gap_role == SCANNER) {
     interval++;
 
-    if ((interval % DETECTION_INTERVAL) == 0) {
+    if ((interval != 0 && interval % DETECTION_INTERVAL) == 0) {
       if (number_of_packets[0] == 0) {
         uint32_t continuous_jamming_detected = 37;
         alert(JAMMING_ALERT_NUMBER,(uint8_t*)&continuous_jamming_detected,4);

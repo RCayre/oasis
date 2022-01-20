9d1a13cd86c9b8ae1457336c8942aa1e3967d94c
RCayre <rcayre@laas.fr>
2022-01-20 22:15:43 +0100

    Cleaning injectable module

--- modules/injectable/module.c
+++ modules/injectable/module.c
@@ -6,8 +6,9 @@
 
 #define WINDOW_SIZE 24
 #define HASHMAP_SIZE 16
-#define THRESHOLD 100
+#define THRESHOLD 10
 
+/* Sorted circular buffer definition */
 typedef struct sorted_circular_buffer {
 	uint32_t tab[WINDOW_SIZE];
 	uint32_t indexes[WINDOW_SIZE];
@@ -20,10 +21,9 @@ typedef struct injectable_data {
   uint32_t last_hop_interval;
 } injectable_data_t;
 
-static hashmap_t * hashmap = NULL;
-uint32_t injectable_detected = 0;
+static hashmap_t * injectable_hashmap = NULL;
 
-
+/* Insert a new value in the sorted circular buffer and removes the oldest one if the buffer is full */
 void insert(sorted_circular_buffer_t* sa, int val) {
 	int old_index = sa->indexes[sa->pointer];
 	int size = (sa->full ? WINDOW_SIZE : sa->pointer);
@@ -64,31 +64,34 @@ void insert(sorted_circular_buffer_t* sa, int val) {
 	if (sa->pointer == 0 && !sa->full) sa->full = true;
 }
 
+/* Returns the median of the sorted circular buffer (if it is full) */
 uint32_t median(sorted_circular_buffer_t* sa) {
   return sa->tab[WINDOW_SIZE/6];
 }
 
-uint32_t val[120];
-uint32_t valp = 0;
-uint32_t value = 0xFFFFFFFF;
-uint32_t vvv = 0;
-uint16_t update = 0;
-
 void CONN_DELETE_CALLBACK(injectable)(metrics_t *metrics) {
-  hashmap_free(&hashmap);
-  hashmap = NULL;
+  log(&injectable_hashmap, 4);
+  hashmap_free(&injectable_hashmap);
+  log(&injectable_hashmap, 4);
+
 }
+
 void CONN_RX_CALLBACK(injectable)(metrics_t * metrics) {
   // This detection is intended to be used as a slave
   if(metrics->local_device->gap_role != PERIPHERAL) {
     return;
   }
-  if(hashmap == NULL) {
-    hashmap = hashmap_initialize(HASHMAP_SIZE, NULL, 4);
+
+  // If the hashmap doesn't exist, creates it
+  if(injectable_hashmap == NULL) {
+    injectable_hashmap = hashmap_initialize(HASHMAP_SIZE, NULL, 4);
   }
-  // Get this device's data for detecting a mitm attack
+
+  // Get the connection data for detecting a injectable attack
   injectable_data_t * data;
-  void * ret = hashmap_get(hashmap, (uint8_t*)&metrics->current_connection->access_address);
+  void * ret = hashmap_get(injectable_hashmap, (uint8_t*)&metrics->current_connection->access_address);
+
+
   if(ret == NULL) {
     // Add an entry if it wasn't found
     data = (injectable_data_t *) malloc(sizeof(injectable_data_t));
@@ -98,42 +101,47 @@ void CONN_RX_CALLBACK(injectable)(metrics_t * metrics) {
     }
     data->window.pointer = 0;
     data->window.full = false;
-    int err = hashmap_put(hashmap, (uint8_t*)&metrics->current_connection->access_address, data);
+    int err = hashmap_put(injectable_hashmap, (uint8_t*)&metrics->current_connection->access_address, data);
+
     // If there was not enough memory, skip
     if(err == -1) {
       return;
     }
-  } else {
+  }
+  // If an entry exists, get it
+  else {
     data = (injectable_data_t*)ret;
   }
-  // Exclude first value
+
+  // Exclude first value or invalid packet
   if(metrics->remote_device->connection_interval == 0 || !metrics->current_packet->valid) {
     return;
   }
 
-	if (data->last_hop_interval != (*((uint32_t*) (0x200007bc))>>16)) {
+  // If a new interval is used, resets the circular buffer
+	if (data->last_hop_interval != metrics->remote_device->connection_interval) {
 		data->window.full = false;
 		data->window.pointer = 0;
 	}
-	data->last_hop_interval = (*((uint32_t*) (0x200007bc))>>16);
-  int ok = 0;
-  if (data->window.full) {
-    uint32_t v = metrics->remote_device->connection_interval;
+	data->last_hop_interval = metrics->remote_device->connection_interval;
 
+  // If the sorted circular buffer is full, performs the detection
+  if (data->window.full) {
+    // get the current interval
+    uint32_t current_interval = metrics->remote_device->connection_interval;
+    // get the median of previous intervals
     uint32_t med = median(&data->window);
-    int32_t tmp = (((v - med/2) % med) - med/2);
-		uint32_t updated = (tmp >= 0 ? tmp : -tmp);
-		updated = (2000*updated) / med;
-    //val[valp] = (updated <= 10 && updated > 0 ? updated : 0);//updated > 0x10 && updated < 0x50;// > 1 ? *((uint32_t*) (0x200007bc))  : value;/*tmp > 20  && value != 0xAAAAAAAA ? metrics->current_packet->header[1] : value;*/
-    //valp = (valp + 1) % 120;
-    if (updated <= 10 && updated > 0) {
-      log(&updated,4);
+
+    int32_t error = (((current_interval - med/2) % med) - med/2);
+		uint32_t absolute_error = (error >= 0 ? error : -error);
+		absolute_error = (2000*absolute_error) / med;
+
+
+    if (absolute_error <= THRESHOLD && absolute_error > 0) {
+      log(&absolute_error,4);
     }
   }
-  else {
-    ok = 1;
-  }
 
-   insert(&data->window, metrics->remote_device->connection_interval);
-
+  // Insert the current interval in the sorted circular buffer
+  insert(&data->window, metrics->remote_device->connection_interval);
 }

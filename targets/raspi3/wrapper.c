7afe7afb64f8bba1e0d5638c8818e2abc9a9d5da
RCayre <rcayre@laas.fr>
2022-01-21 17:47:12 +0100

    Implementing callback TX done in raspi3 target

--- targets/raspi3/wrapper.c
+++ targets/raspi3/wrapper.c
@@ -222,7 +222,13 @@ void on_scan_rx() {
 #ifdef CONNECTION_ENABLED
 
 
+
 // Connection-related hooks
+
+void on_conn_tx() {
+  process_conn_tx();
+}
+
 void on_conn_rx_header() {
     process_conn_rx_header();
 }

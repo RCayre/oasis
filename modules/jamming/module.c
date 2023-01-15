bcf59bc3b3456ec8e46d7718c104221eb7669a34
RCayre <rcayre@laas.fr>
2023-01-15 15:26:27 +0100

    Improving jamming module to collect invalid frames

--- modules/jamming/module.c
+++ modules/jamming/module.c
@@ -6,14 +6,18 @@
 #include "alert.h"
 
 #define JAMMING_ALERT_NUMBER 5
-#define DETECTION_INTERVAL 25
+#define DETECTION_INTERVAL 10
 
 uint32_t interval;
 uint32_t number_of_packets[3];
+uint32_t number_of_invalid_packets[3];
 
 void SCAN_CALLBACK(jamming)(metrics_t * metrics) {
   if (metrics->current_packet->valid) {
     number_of_packets[metrics->current_packet->channel - 37]++;
+  }
+  else {
+    number_of_invalid_packets[metrics->current_packet->channel - 37]++;
   }
 }
 
@@ -22,6 +26,12 @@ void TIME_CALLBACK(jamming)(metrics_t * metrics) {
     interval++;
 
     if ((interval != 0 && interval % DETECTION_INTERVAL) == 0) {
+
+      uint8_t report[24];
+      memcpy(report,number_of_packets, 12);
+      memcpy(report+12,number_of_invalid_packets, 12);
+      log(report, 24);
+
       if (number_of_packets[0] == 0) {
         uint32_t continuous_jamming_detected = 37;
         alert(JAMMING_ALERT_NUMBER,(uint8_t*)&continuous_jamming_detected,4);
@@ -38,6 +48,11 @@ void TIME_CALLBACK(jamming)(metrics_t * metrics) {
       number_of_packets[0] = 0;
       number_of_packets[1] = 0;
       number_of_packets[2] = 0;
+
+      number_of_invalid_packets[0] = 0;
+      number_of_invalid_packets[1] = 0;
+      number_of_invalid_packets[2] = 0;
+
     }
 
   }
@@ -45,6 +60,10 @@ void TIME_CALLBACK(jamming)(metrics_t * metrics) {
     number_of_packets[0] = 0;
     number_of_packets[1] = 0;
     number_of_packets[2] = 0;
+
+    number_of_invalid_packets[0] = 0;
+    number_of_invalid_packets[1] = 0;
+    number_of_invalid_packets[2] = 0;
 
     interval = 0;
   }
